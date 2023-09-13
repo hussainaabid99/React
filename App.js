239bcd9453fd19882f1fabb4c369c216662ec8e9
Aabid <hussain.aabid2002@gmail.com>
2023-09-13 12:42:54 +0530

    Added babbel, Function components,JSX

--- App.js
+++ App.js
@@ -4,53 +4,121 @@ import ReactDOM from "react-dom/client";
 /***
  *  !Parcel features
  *
- * Hot Module Reloading
- * File Watcher Algorithm - C++
- * Bundling
- * Minification
- * Cleaning Code
- * Dev and Production Build
- * Super Fast build algorithm
- * Image Optimization
- * Caching while development
- * Compression
- * Compatible with older version of browser
- * HTTPS on dev machine
- * Manages Port Number
- * Consistent Hashing Algorithms
- * Zero Config
+ ** Hot Module Reloading
+ ** File Watcher Algorithm - C++
+ ** Bundling
+ ** Minification
+ ** Cleaning Code
+ ** Dev and Production Build
+ ** Super Fast build algorithm
+ ** Image Optimization
+ ** Caching while development
+ ** Compression
+ ** Compatible with older version of browser
+ ** HTTPS on dev machine
+ ** Manages Port Number
+ ** Consistent Hashing Algorithms
+ ** Zero Config
+ ** Tree Shaking - Removing un-wanted
  * 
- * 
- * 
- * Transitive Dependencies
+ *  
+ ** Transitive Dependencies
  */
 
-const heading = React.createElement("h1", {
-          id: "title",
-},
-          "Heading 1 for parcel"
-);
 
-const heading2 = React.createElement(
-          "h2",
-          {
-                    id: "title",
-          },
-          "Heading 2"
-);
+//React.createElement gives us an Object
 
-const container = React.createElement(
-          "div",
-          {
-                    id: "container",
-          },
-          [heading, heading2]
-);
+/**<div class="header">
+                    <h1>Hello World</h1>
+                    <ul>
+                              <li>About us</li>
+                              <li>Support</li>
+                              <li>Home</li>
+                    </ul>
+          </div> */
 
-console.log(heading);
+
+// const container = React.createElement(
+//           "div",
+//           {
+//                     id: "container",
+
+//           },
+//           [React.createElement(
+//                     "h1",
+//                     {
+//                               id: "title",
+//                               key: "h2"
+//                     },
+//                     "Hello World"
+
+//           ), React.createElement(
+//                     "ul",
+//                     {},
+//                     [React.createElement(
+//                               "li",
+//                               {},
+//                               "About us"
+//                     ),
+//                     React.createElement(
+//                               "li",
+//                               {},
+//                               "Support"
+//                     ),
+//                     React.createElement(
+//                               "li",
+//                               {},
+//                               "Home"
+//                     )]
+//           )]
+// );
+
+//! JSX 
+
+const heading2 = (                      //JSX Expression
+          <h1 id="title" key="h2" tabIndex="1" className="">
+                    Hello World
+          </h1>);
+
+
+//* Babel converts JSX => React.createElement 
+//* Then React.createElement => Object => HTML */
+
+
+// ! React Components
+//        - Functional Components      (New)
+//        - Class Based Components - (Old Way)
+
+
+// * Functional Components
+
+//Normal fn which returns a react element/JSX
+//Name of FC starts with capital letter (not mandatory)
+
+const HeaderComponent2 = () => {
+
+          <div>
+                    <h2>Functional Component</h2>
+                    <h2>This is a h2 tags</h2>
+          </div>
+
+};
+const HeaderComponent = () => {
+          return (
+                    <div>
+                              {heading2}
+                              <HeaderComponent2 />
+                              <h2>Functional Component</h2>
+                              <h2>This is a h2 tags</h2>
+                    </div>
+          );
+};
+
+
+//Both are valid
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //passing a react element inside the root 
-root.render(container);
+root.render(<HeaderComponent />);
 
